e7f3b4a6b2c049cdfd8e2900e09be3a0ec7bd18d
labrame <labrame.blog@gmail.com>
2016-04-07 21:46:17 +0100

    added contribs and extensions

--- DatabaseAdministration.sql
+++ DatabaseAdministration.sql
@@ -73,3 +73,23 @@ GRANT SELECT, REFERENCES, TRIGGER
 
 GRANT EXECUTE ON ALL FUNCTION IN SCHEMA contrib TO public;
 GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA contrib TO public;
+
+--extension and contribs
+--installed separatly in each database
+--(use template to install extension under predefined specific schema)
+
+--list of extension
+SELECT *
+FROM pg_available_extensions
+WHERE comment LIKE '%string%' OR installed_version IS NOT NULL
+ORDER BY name;
+
+--get the details about an extension in psql (not tested)
+\dx+ fuzzystrmatch
+
+--or run the query
+SELECT pg_catalog.pg_describe_object(d.classid, d.objid, 0) AS description
+FROM pg_catalog.pg_depend AS D
+	INNER JOIN pg_extension AS E ON D.refobjid = E.oid
+WHERE D.refclassid = 'pg_catalog.pg_extension'::pg_catalog.regclass
+	AND deptype = 'e'
